6c8bd109df8344407a85446ddee9365169062c68
Aryan Naraghi <aryan.naraghi@gmail.com>
2021-02-22 19:35:13 -0800

    Adds a simple component.

---
+++ app/src/app/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+
+  getItems(): string {
+    return this.route.snapshot.paramMap.get('id') || '';
+  }
+}
