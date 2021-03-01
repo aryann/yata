1cce802abdf30184f23c5ccd15119bd17544ad70
Aryan Naraghi <aryan.naraghi@gmail.com>
2021-02-28 16:56:45 -0800

    Adds some prerequisites for hosting.

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCSKNcL6ekfUSx4OlWpsMbdrE84EG7eujI',
+    authDomain: 'yata.aryan.app',
+    projectId: 'yata-96c4d',
+    storageBucket: 'yata-96c4d.appspot.com',
+    messagingSenderId: '148886328864',
+    appId: '1:148886328864:web:12873101622ce9b01f8e45',
+    measurementId: 'G-CNMCBE196G',
+  },
 };
